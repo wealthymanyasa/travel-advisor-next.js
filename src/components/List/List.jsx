a2b34487434028ec3f71d8b7c88d9c06baa44f2e
wealthymanyasa <omanyasa@yahoo.com>
2022-01-21 12:04:39 +0200

    second commit

---
+++ src/components/List/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const List = () => {
+
+    return(
+        <h1>list</h1>
+    );
+}
+
+export default List;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//main imports
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//importing other files
+import App from './App';
+
+ReactDom.render(<App />, document.getElementById('root'));
+
