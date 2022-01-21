a578633df2ee3d73444bd69c21797f1ff77bdc27
wealthymanyasa <omanyasa@yahoo.com>
2022-01-21 17:29:32 +0200

    added google-map-react

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper , Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab'; 
+import useStyles from './styles.js';
+
+
 
 const Map = () => {
+      const classes = useStyles();
+      const isMobile = useMediaQuery('(min-width:600px)');
+
+      const coordinates = {lat:0, lng: 0};
 
     return(
-        <h1>map</h1>
-    );
+        <div className={classes.mapContainer} >
+                <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyDnNcAaBAg-RZdxPVDzi2ydern55rmL5OY'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+                >
+                    
+                </GoogleMapReact>
+    
+        </div> 
+       );
 }
 
 export default Map;
